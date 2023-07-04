f9eba46eec394955f07baee1dc4ce815e0b1bcb3
Veronica dos Santos <46487949+versant2612@users.noreply.github.com>
2023-07-04 01:25:52 -0300

    local function import pygeos
    To compute intersection between two polygons

---
+++ H4/geo_functions.py
@@ -0,0 +1,16 @@
+import pygeos as pg
+from pygeos import Geometry
+
+def geo_intersection(geo1, geo2):
+
+  pol1 = pg.Geometry(geo1)
+  pol2 = pg.Geometry(geo2)
+
+  return str(pg.intersection(pol1, pol2))
+
+def geo_intersects(geo1, geo2):
+
+  pol1 = pg.Geometry(geo1)
+  pol2 = pg.Geometry(geo2)
+
+  return str(pg.intersects(pol1, pol2))
